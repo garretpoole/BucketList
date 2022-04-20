9bc3fbcc5da8541cb767c5c7331e964c149faf16
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-20 02:35:29 -0700

    Comparable conformance for custom data types

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstname: String
+    let lastname: String
+    //built in comparable method (< operator) for .sorted()
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastname < rhs.lastname
+    }
+}
+
 struct ContentView: View {
+    let values = [1, 5, 3, 6, 2, 9].sorted()
+    let users = [
+        User(firstname: "Arnold", lastname: "Rimmer"),
+        User(firstname: "Krisitine", lastname: "Kochanski"),
+        User(firstname: "David", lastname: "Lister")
+    ].sorted()
+//  can sort with closure but not efficient or clean when having to sort multiple data times
+//        .sorted {
+//        $0.lastname < $1.lastname
+//    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(users) { user in
+            Text("\(user.firstname) \(user.lastname)")
+        }
     }
 }
 
