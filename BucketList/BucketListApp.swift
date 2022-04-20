b6f210ab31c28eca96bb75d766314ab26234eed6
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-20 02:23:16 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Garret Poole on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
