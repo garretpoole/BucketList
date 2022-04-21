ec6ae158aec67c156aea4ff0c27e0349cf8a8379
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-20 23:14:42 -0700

    MapKit with interaction for POIs

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,46 +5,37 @@
 //  Created by Garret Poole on 4/20/22.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-struct SuccessView: View {
-    var body: some View {
-        Text("Sucess!")
-    }
-}
-struct FailedView: View {
-    var body: some View {
-        Text("Failed")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    var loadingState = LoadingState.loading
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                    Circle()
+                        .stroke(.red, lineWidth: 3)
+                        .frame(width: 22, height: 22)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
         }
-//        if loadingState == .loading {
-//            LoadingView()
-//        } else if loadingState == .success {
-//            SuccessView()
-//        } else if loadingState == .failed {
-//            FailedView()
-//        }
     }
 }
 
