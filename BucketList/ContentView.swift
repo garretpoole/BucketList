dcf00e05095c35b9fd7c8749bf4af6032aed8f4d
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-21 01:27:39 -0700

    FaceID and TouchID biometrics for unlocking secure data

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -3,38 +3,42 @@
 //  BucketList
 //
 //  Created by Garret Poole on 4/20/22.
-//
 
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var isUnlocked = false
     
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
     var body: some View {
-        NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                    Circle()
-                        .stroke(.red, lineWidth: 3)
-                        .frame(width: 22, height: 22)
-                    }
-                }
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
-            .navigationTitle("London Explorer")
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            //string used with touchID other is used for faceID
+            let reason = "We need to unlock your data"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    //authenticated succesfully
+                    isUnlocked = true
+                } else {
+                    //there is a problem
+                }
+                
+            }
+        } else {
+            //no biometrics
         }
     }
 }
