da885560e33f92e483f31c84f8bb7039a995fe38
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-22 02:31:14 -0700

    Lock UI behind face/touch ID

--- BucketList/ContentView-ViewModel.swift
+++ BucketList/ContentView-ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 //MVVM (model view view-model) design pattern implemented
@@ -18,6 +19,8 @@ extension ContentView {
         @Published private(set) var locations: [Location]
         //for editing an already marked location
         @Published var selectedPlace: Location?
+        //check for if phone is unlocked
+        @Published var isUnlocked = false
         
         //used for reading and writing files with the same file in both places
         //data is saved on disc (takes up phone storage) and much more flexible than UserDefaults
@@ -56,6 +59,30 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                //used for touchID
+                let reason = "Please authenticate yourself to view your places"
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    if success {
+                        //start new task to run in main actor to queue up work to make isUnlocked true
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    }
+                    else {
+                        //error
+                    }
+                }
+            } else {
+                //no biometrics available
             }
         }
         
